0792b68f44f6b6de5bdb5786c625e06a6ed7eaf2
Alex Thelen <alexander.e.thelen@gmail.com>
2014-10-19 10:11:25 -0400

    SimulationEngine now inherits from ProcessingComponent. Fixed build errors in Demo.java

--- HeatedEarth/src/EarthSim/SimulationEngine/SimulationEngine.java
+++ HeatedEarth/src/EarthSim/SimulationEngine/SimulationEngine.java
@@ -1,12 +1,12 @@
 package EarthSim.SimulationEngine;
 import EarthSim.GUI.DataBuffer;
+import EarthSim.ProcessingComponent;
 
-public class SimulationEngine implements Runnable 
+public class SimulationEngine extends ProcessingComponent
 {
 	//Attributes--------------------------
 	private Planet earth;
 	private int _gridSize;
-	private int _hours;
 	private int _minutesPerRotation;
 	private DataBuffer _buffer;
 	private boolean produce;
@@ -14,16 +14,15 @@ public class SimulationEngine implements Runnable
 	//Accessors---------------------------
 	public int GetGridSize() { return this._gridSize; }
 	public void SetGridSize(int value) { this._gridSize = value; }
-	public int GetRunHours() { return this._hours; }
-	public void SetRunHours(int value) { this._hours = value; }
 	public int GetMinutesPerRotation() { return this._minutesPerRotation; }
 	public void SetMinutesPerRotation(int value) { this._minutesPerRotation = value; }
+	public boolean GetProduce(){ return this.produce; }
+	public void SetProduce(boolean value){ this.produce = value; }
 
 	//Constructors------------------------
-	public SimulationEngine(DataBuffer buffer, int cellSize, int hours, int minutesPerRotation)
+	public SimulationEngine(DataBuffer buffer, int cellSize, int minutesPerRotation)
 	{
 		this._gridSize = cellSize;
-		this._hours = hours;
 		this._minutesPerRotation = minutesPerRotation;
 		this.produce = false;
 		
@@ -38,14 +37,7 @@ public class SimulationEngine implements Runnable
 	}
 	
 	//Public Methods----------------------
-	@Override
-	public void run() 
-	{
-		
-	}
-
-	//Private Methods---------------------
-	private void RunSimulation()
+	public void RunSimulation()
 	{
 		try 
 		{
@@ -61,4 +53,7 @@ public class SimulationEngine implements Runnable
 			e.printStackTrace();
 		}
 	}
+	
+	//Private Methods---------------------
+
 }
