ab890b53ed416e056edb19f22c801f2ae55aabb5
Alex Thelen <alexander.e.thelen@gmail.com>
2014-10-25 14:47:40 -0400

    Directory used on all configs now. Updated results

--- runTest.sh
+++ runTest.sh
@@ -12,23 +12,28 @@
 # Param =4 : GUI, SE, Pres on seperate thread, SE with initiative
 # Param =5 : GUI, SE, Pres on 1 thread, Pres with initiative
 # Param =6 : GUI, SE, Pres on seperate thread, Pres with initiative
+# Param =7 : 
+# Param =8 : 
+# Param =9 : 
+# Param =10 : 
 
 cd HeatedEarth
 cd bin
 
 x="$1"
 b="$2"
+directory="/home/ubuntu/git/CS6310_P2_T11/Reports/Test Results/Grid Spacing_15 Time Step_1"
 
 if [ $x -eq 1 ] 
-then java EarthSim.Demo -b$b >> /home/ubuntu/git/CS6310_P2_T11/Reports/"Test Results"/"Grid Spacing_15 Time Step_1"/results_GUI1.csv
+then java EarthSim.Demo -b$b >> $directory/results_GUI1.csv
 elif [ $x -eq 2 ] 
-then java EarthSim.Demo -s -p -b$b >> results_GUI3.csv
+then java EarthSim.Demo -s -p -b$b >> $directory/results_GUI3.csv
 elif [ $x -eq 3 ] 
-then java EarthSim.Demo -t -b$b >> results_SE1.csv
+then java EarthSim.Demo -t -b$b >> $directory/results_SE1.csv
 elif [ $x -eq 4 ] 
-then java EarthSim.Demo -s -p -t -b$b >> results_SE3.csv
+then java EarthSim.Demo -s -p -t -b$b >> $directory/results_SE3.csv
 elif [ $x -eq 5 ] 
-then java EarthSim.Demo -r -b$b >> results_P1.csv
+then java EarthSim.Demo -r -b$b >> $directory/results_P1.csv
 elif [ $x -eq 6 ] 
-then java EarthSim.Demo -s -p -r -b$b >> results_P3.csv
+then java EarthSim.Demo -s -p -r -b$b >> $directory/results_P3.csv
 fi
